617ba502e64d5836d17753bf7c2508a1410406c8
root <root@localhost.localdomain>
2019-05-07 22:56:03 +0200

    impr

--- Matrix.py
+++ Matrix.py
@@ -5,11 +5,10 @@ class MatrixNxN:
 	def __init__(self,*args):
 		self.matrix=[]
 		args_elements=[]
-		self.matrix_length=[i for i in args]
 		
-		#if math.sqrt(len(args))-int(math.sqrt(len(args))):
-		#	print('Matrix should be a N X N !!!')
-		#	return 1
+		if math.sqrt(len(args))-int(math.sqrt(len(args))):
+			print('Matrix should be a N X N !!!')
+			return 1
 
 		for value in args:
 			args_elements.append(value)
@@ -19,10 +18,10 @@ class MatrixNxN:
 
 
 	def __add__(self,other):
-		if isinstance(other, int):
-			return MatrixNxN(*(egalplus(self.matrix,other)))
+		if isinstance(other, MatrixNxN):
+			return MatrixNxN(*(sft(self.matrix,other.matrix)))
 		else:
-		     return MatrixNxN(*(sft(self.matrix,other.matrix)))
+		     return MatrixNxN(*(egalplus(self.matrix,other)))
 
 	def add(self,other):
 		return self.__add__(other)
@@ -38,19 +37,19 @@ class MatrixNxN:
 		
 
 	def __mul__(self, other):
-		if isinstance(other, (int)):
-			return MatrixNxN(*(egalmul(self.matrix,other)))
+		if isinstance(other, MatrixNxN):
+			return MatrixNxN(*(prodh(self.matrix,other.matrix)))
 		else:
-		     return MatrixNxN(*(prodh(self.matrix,other.matrix)))
+		     return MatrixNxN(*(egalmul(self.matrix,other)))
 	def __iadd__(self,other):
 		return self.__add__(other)
 	def __imul__(self,other):
 		return self.__mul__(other)
 	def __sub__(self,other):
-		if isinstance(other, int):
-			return MatrixNxN(*(egalminus(self.matrix,other)))
+		if isinstance(other, MatrixNxN):
+			return MatrixNxN(*(sftminus(self.matrix,other.matrix)))
 		else:
-		     return MatrixNxN(*(sftminus(self.matrix,other.matrix)))
+		     return MatrixNxN(*(egalminus(self.matrix,other)))
 	def __isub__(self,other):
 		return self.__sub__(other)
 def prodh(matrix,omatrix):
@@ -78,24 +77,24 @@ def sft(matrix,omatrix):
 
 def egalplus(matrix,value):
 	c=[]
-	if isinstance(value, int):
-		for i in range(len(matrix)):
-			for j in range(len(matrix[0])):
-				c.append(matrix[i][j]+value)
+	
+	for i in range(len(matrix)):
+		for j in range(len(matrix[0])):
+			c.append(matrix[i][j]+value)
 	return c
 def egalmul(matrix,value):
 	c=[]
-	if isinstance(value, int):
-		for i in range(len(matrix)):
-			for j in range(len(matrix[0])):
-				c.append(matrix[i][j]*value)
+	
+	for i in range(len(matrix)):
+		for j in range(len(matrix[0])):
+			c.append(matrix[i][j]*value)
 	return c
 def egalminus(matrix,value):
 	c=[]
-	if isinstance(value, int):
-		for i in range(len(matrix)):
-			for j in range(len(matrix[0])):
-				c.append(matrix[i][j]-value)
+	
+	for i in range(len(matrix)):
+		for j in range(len(matrix[0])):
+			c.append(matrix[i][j]-value)
 	return c
 def sftminus(matrix,omatrix):
 	if len(matrix) != len(omatrix):
--- task.py
+++ task.py
@@ -1,8 +1,13 @@
 from Matrix import MatrixNxN
 
 def main():
+	matrix0=MatrixNxN()
+	matrix1x1=MatrixNxN(1)
+	matrix2x2=MatrixNxN(-2.5,6.6,0.66,3.4)
+	matrix4x4=MatrixNxN(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
 	
-	
+	#matrix5x5 --> matrixNxN
+	print(matrix2x2)
 
 
 	a=MatrixNxN(1,2,3,4,5,6,7,8,9)
@@ -34,6 +39,11 @@ def main():
 
 	c-=b+55
 	c.display()
+
+	c=b*7
+	print(c)
+	c*=7
+	print(c)
 
 
 if __name__=='__main__':
